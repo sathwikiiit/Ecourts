e23663fa45dd7bc2e6240df9263dbdfe0b3e9804
Sathwikiiit <sathwik.sns@gmail.com>
2025-09-22 16:14:41 +0530

    api key access

--- src/lib/actions/utils.ts
+++ src/lib/actions/utils.ts
@@ -1,6 +1,8 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export const API_BASE_URL = 'https://court-api.kleopatra.io/api/core';
-export const API_KEY = process.env.COURT_API_KEY;
+//@ts-expect-error -- IGNORE ---
+export const API_KEY = getRequestContext().env.COURT_API_KEY;
 
 export const getAuthHeaders = () => {
     const headers: HeadersInit = {
